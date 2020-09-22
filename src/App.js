af02c247a9052bfdc190e1c15aec734191365983
Chuck Thompson <chuckthompson27@me.com>
2020-09-22 16:12:28 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const totalBoardRows = 40;
+const totalBoardColumns = 60;
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <h1> Game of Life </h1>
+    );
+  }
 }
 
 export default App;
